f8dda81037ceea87902a72d854a7456f33ee37c5
Harry Sun <sunguangjun@126.com>
2016-03-04 17:14:54 +0800

    添加 ES6 的例子

---
+++ es6/let.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var a = [];
+for (var i = 0; i < 10; i++) {
+  a[i] = function () {
+    console.log(i);
+  };
+}
+a[6](); // 10
+
+var a = [];
+for (let i = 0; i < 10; i++) {
+  a[i] = function () {
+    console.log(i);
+  };
+}
+a[6](); // 6
