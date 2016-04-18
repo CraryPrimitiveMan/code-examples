e5882cc03a721bdf28e195d23cbe2aa5172df911
Harry Sun <harrysun@augmentum.com.cn>
2016-04-18 19:03:56 +0800

    添加 php 数据处理的例子

---
+++ php/cmn.php
@@ -0,0 +1,84 @@
+<?php
+$arr = [
+	[
+		'id' => 'bb340471-53e4-1591-3238-d6ccf0d930a1',
+		'properties' => [
+			[
+				'id' => 1234,
+				'name' => '1'
+			],
+			[
+				'id' => 1235,
+				'name' => '2'
+			],
+			[
+				'id' => 1236,
+				'name' => '3'
+			]
+		]
+	],
+	[
+		'id' => 'bb340471-53e4-1591-3238-d6ccf0d930a2',
+		'properties' => [
+			[
+				'id' => 1237,
+				'name' => '4'
+			],
+			[
+				'id' => 1238,
+				'name' => '5'
+			],
+			[
+				'id' => 1239,
+				'name' => '6'
+			],
+			[
+				'id' => 1240,
+				'name' => '7'
+			]
+		]
+	],
+	[
+		'id' => 'bb340471-53e4-1591-3238-d6ccf0d930a3',
+		'properties' => [
+			[
+				'id' => 1241,
+				'name' => '8'
+			],
+			[
+				'id' => 1242,
+				'name' => '9'
+			],
+			[
+				'id' => 1243,
+				'name' => '10'
+			],
+			[
+				'id' => 1244,
+				'name' => '11'
+			]
+		]
+	]
+];
+
+$result = [];
+foreach ($arr as $index => $value) {
+  $result[$index] = [];
+  foreach ($value['properties'] as $propertie) {
+    $result[$index][] = $propertie['id'];
+  }
+}
+
+$a = array_reduce($result, function($pre, $next) {
+  $temp = [];
+  if (empty($pre)) {
+    return $next;
+  }
+  foreach ($pre as $key1 => $value1) {
+    foreach ($next as $key2 => $value2) {
+      $temp[] = $value1 . ',' . $value2;
+    }
+  }
+  return $temp;
+});
+var_dump($a);
