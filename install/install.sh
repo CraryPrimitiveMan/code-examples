4a3b881d1b1bd94291354f1ac2b3e27b8e4eb52b
Harry Sun <harrysun@augmentum.com.cn>
2016-06-03 15:36:18 +0800

    修正 install 脚本

--- install/install.sh
+++ install/install.sh
@@ -2,6 +2,7 @@ softwares=(
 lantern
 chromium
 git
+zeal
 
 lua
 nodejs
@@ -17,6 +18,7 @@ for name in ${softwares[@]}
 do
   if [[ -d ${path}/${name} ]] && [[ -f ${path}/${name}/install.sh ]]; then
     # sudo chmod +x ${path}/${name}/install.sh
-    ${path}/${name}/install.sh
+    cd ${path}/${name}/
+    ./install.sh
   fi
 done
